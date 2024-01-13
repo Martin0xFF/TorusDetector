b064f545b2523d89f95e2c0dbf058dcdfa87cfba
Tony Tran <trant8381@wrdsb.ca>
2024-01-13 17:06:32 -0500

    added training loss

--- train.py
+++ train.py
@@ -14,8 +14,9 @@ from PIL import Image, ImageDraw
 
 
 class TrainOptions:
-    def __init__(self, tl, md, lf, opt, tb, log):
-        self.training_loader = tl
+    def __init__(self, train, test, md, lf, opt, tb, log):
+        self.training_loader = train
+        self.testing_loader = test
         self.model = md
         self.loss_fn = lf
         self.optimizer = opt
@@ -30,6 +31,7 @@ class TrainRig:
         self.model = self.options.model
         self.loss_fn = self.options.loss_fn
         self.training_loader = self.options.training_loader
+        self.testing_loader = self.options.testing_loader
         self.optimizer = self.options.optimizer
         self.log = self.options.log
         self.tb_writer = self.options.tb_writer
@@ -37,6 +39,7 @@ class TrainRig:
     def train(self, epochs):
         for epoch in range(epochs):
             self._train_one_epoch(epoch)
+            self.test()
 
     def _train_one_epoch(self, epoch_index):
         batch_loss = 0.0
@@ -61,6 +64,20 @@ class TrainRig:
             debug_loss = 0.0
         return batch_loss
 
+    def test(self):
+        batch_loss = 0.0
+        for i, data in enumerate(self.testing_loader):
+            inputs, labels = data
+            print(labels.shape)
+            print(model(inputs).shape)
+            # Compute the loss and its gradients
+            loss = self.loss_fn(model(inputs), labels)
+
+            batch_loss += loss
+        if self.log:
+            avg_loss = batch_loss / (i + 1)  # loss per batch
+            print(f"\nTest loss: {avg_loss}")
+        return batch_loss
 
 def rename_images(path_glob="data/*bayer*.png", new_name="bayer", start_index=0):
     # Rename images from data set.
@@ -150,7 +167,7 @@ def Inspect(model, path_glob="data/*color*.png"):
 
 def AutoAnnotate(model, path_glob="data/*color*.png"):
     found_images = glob.glob(path_glob)
-    boxes = []
+    boxes = {}
 
     for i, img_path in enumerate(found_images):
         print(f"Viewing Image: {i}")
@@ -159,13 +176,38 @@ def AutoAnnotate(model, path_glob="data/*color*.png"):
 
         if (out_box < 0).any():
             print(f"Skipping: {img_path} because bounds are negative.")
-            boxes.append([-1, -1, -1, -1])
+            boxes[img_path] = [-1, -1, -1, -1]
             continue
         else:
-            boxes.append([int(l) for l in out_box])
+            boxes[img_path] = [int(l) for l in out_box]
 
     with open("output/boxes.json", "w") as file:
         file.write(json.dumps(boxes))
+
+def Review(path_glob="data/*color*.png"):
+    boxes = {}
+
+    with open("output/boxes.json", "r") as file:
+        boxes = json.load(file)
+
+    new_boxes = {}
+    for img_path, box in boxes.items():
+        if box[0] < 0:
+            print(f"Skipping: {img_path} because bounds are negative.")
+        else:
+            with Image.open(img_path) as im:
+                draw_handle = ImageDraw.Draw(im)
+                draw_handle.rectangle(((box[0], box[1]), (box[0] + box[2], box[1] + box[3])), outline="red")
+                im = im.rotate(180, Image.NEAREST, expand=1)
+                im.show()
+                i = input("Keep? [Y/n]")
+
+                if (i == "Y" or i == ""):
+                    im.save(img_path.replace("data", "output"))
+                    new_boxes[img_path] = box
+
+    with open("output/boxes.json", "w") as file:
+        file.write(json.dumps(new_boxes))
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -176,7 +218,7 @@ if __name__ == "__main__":
         "-t",
         "--task",
         required=True,
-        choices=["train", "inspect", "auto-annotation", "rename"],
+        choices=["train", "inspect", "auto-annotation", "review", "rename"],
     )
     args = parser.parse_args()
 
@@ -192,10 +234,13 @@ if __name__ == "__main__":
         annotations = None
         with open(json_path) as js:
             annotations = json.load(js)
-        tloader = DataLoader(TorusData(annotations), batch_size=20)
+
+        training_loader = DataLoader(TorusData(dict(list(annotations.items())[20:])), batch_size=20)
+        testing_loader = DataLoader(TorusData(dict(list(annotations.items())[:20])), batch_size=20)
 
         to = TrainOptions(
-            tloader,
+            training_loader,
+            testing_loader,
             model,
             torch.nn.MSELoss(),
             opt,
@@ -215,11 +260,12 @@ if __name__ == "__main__":
         Inspect(st)
 
     elif args.task == "auto-annotation":
-        # TODO(ttran): Support auto annotation feature
         st = SingleTorus()
         st.load_state_dict(torch.load("model.pt"))
         st.eval()
         AutoAnnotate(st)
+    elif args.task == 'review':
+        Review()
     elif args.task == "rename":
         rename_images(
             path_glob="new_data/*color*.png",
